8fc004587cd4959581180075e84cac64d6381104
Alok kannaujia <alokabc25@gmail.com>
2024-10-18 18:22:18 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,17 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "vyyxkhxdwzicmbvfxcud.supabase.co",
-//         port: "",
-//         pathname: "/storage/v1/object/public/cabin-images/**",
-//       },
-//     ],
-//   },
-//   // output: "export",
-// };
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "vyyxkhxdwzicmbvfxcud.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+    "rules": { "react/no-unescaped-entities": 0 }
+  },
+  // output: "export",
+};
 
-// export default nextConfig;
+export default nextConfig;
